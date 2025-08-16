db6515bbb13c4c074758b94d0243cbd9ee86a265
Akash <akabhowmick@gmail.com>
2025-08-15 22:20:34 -0400

    add initial files for sk and sing

---
+++ src/utils/Chapters_Info/Chapter_04_Singapore.ts
@@ -0,0 +1,101 @@
+import type { Chapter } from "../../types/book-types";
+
+import singapore0 from "/src/assets/Chapters/01_Japan/1JP0.png";
+import singapore1 from "/src/assets/Chapters/01_Japan/1JP1.png";
+import singapore2 from "/src/assets/Chapters/01_Japan/1JP2.png";
+import singapore3 from "/src/assets/Chapters/01_Japan/1JP3.png";
+import singapore4 from "/src/assets/Chapters/01_Japan/1JP4.png";
+import singapore5 from "/src/assets/Chapters/01_Japan/1JP5.png";
+
+import singapore6 from "/src/assets/Chapters/01_Japan/1JP6.png";
+import singapore7 from "/src/assets/Chapters/01_Japan/1JP7.png";
+import singapore8 from "/src/assets/Chapters/01_Japan/1JP8.png";
+import singapore9 from "/src/assets/Chapters/01_Japan/1JP9.png";
+import singapore10 from "/src/assets/Chapters/01_Japan/1JP10.png";
+
+import singapore11 from "/src/assets/Chapters/01_Japan/1JP11.png";
+import singapore12 from "/src/assets/Chapters/01_Japan/1JP12.jpeg";
+import singapore13 from "/src/assets/Chapters/01_Japan/1JP13.png";
+import singapore14 from "/src/assets/Chapters/01_Japan/1JP14.png";
+
+const page1 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page2 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page3 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page4 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page5 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page6 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page7 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page8 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page9 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page10 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page11 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page12 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page13 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+const page14 =
+  "Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.";
+
+export const singaporeChapter: Chapter = {
+  id: 2,
+  title: "Chapter 1: Blossoms of Japan",
+  content: `Zara and Zen’s first stop was Japan, just in time for cherry blossom season. They strolled through Kyoto’s historic streets, marveled at the golden Kinkaku-ji temple, and shared sweet mochi under blooming sakura trees. They dressed in tiny dog-sized kimonos and bowed politely to locals, eager to learn every bit of the culture. One highlight? Watching real sumo wrestlers train—Zen even tried to mimic them, much to Zara's amusement.`,
+  publishedAt: "2025-07-03",
+  image: singapore0,
+  pages: [
+    page1,
+    page2,
+    page3,
+    page4,
+    page5,
+    page6,
+    page7,
+    page8,
+    page9,
+    page10,
+    page11,
+    page12,
+    page13,
+    page14,
+  ],
+  images: [
+    singapore1,
+    singapore2,
+    singapore3,
+    singapore4,
+    singapore5,
+    singapore6,
+    singapore7,
+    singapore8,
+    singapore9,
+    singapore10,
+    singapore11,
+    singapore12,
+    singapore13,
+    singapore14,
+  ],
+};
