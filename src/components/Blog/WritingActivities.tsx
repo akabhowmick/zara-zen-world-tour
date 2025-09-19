663948bf48b7e8d9bcb2287445421850225a2041
Akash <akabhowmick@gmail.com>
2025-09-19 14:00:07 -0400

    style: update writing activities

--- src/components/Blog/WritingActivities.tsx
+++ src/components/Blog/WritingActivities.tsx
@@ -5,7 +5,7 @@ export const WritingActivities = () => {
   return (
     <>
       {writingActivities.map(({ title, description, to, cta }) => (
-        <div key={to} className="bg-white rounded-lg p-4 shadow text-center">
+        <div key={to} className="bg-white rounded-lg p-4 shadow text-center hover:scale-105">
           <h3 className="font-bold text-xl mb-2">{title}</h3>
           <p className="text-gray-700 mb-4">{description}</p>
           <Link to={to} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
